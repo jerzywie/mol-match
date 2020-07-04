22f13a54a02aa7c9d97c175b6deed86d15765b1e
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2020-07-04 12:44:03 +0100

    Initial commit

---
+++ test/mol_match/core_test.clj
@@ -0,0 +1,53 @@
+(ns mol-match.core-test
+  (:require [clojure.test :refer [are is deftest testing]]
+            [mol-match.core :as SUT]))
+
+(deftest matched-pair?-test
+  (testing "true-matches"
+    (are [t b] (true? (SUT/matched-pair? t b))
+      \C \N
+      \N \C))
+
+  (testing "false-matches"
+    (are [t b] (false? (SUT/matched-pair? t b))
+      \C \C
+      \N \N
+      \x \C
+      \C \x
+      \x \N
+      \N \x
+      \x \x
+      \p \q)))
+
+(deftest count-matches-helper-test
+  (is (= ['(true true) 2] (SUT/count-matches-helper SUT/top-a SUT/bot-a)))
+  (is (= ['(true false) 1] (SUT/count-matches-helper SUT/top-b SUT/bot-b)))
+  (is (= ['(true false) 1] (SUT/count-matches-helper SUT/top-a SUT/bot-b))))
+
+(deftest count-matches-test
+  (is (= ['(true true) 2] (SUT/count-matches "A" "A")))
+  (is (= ['(true false) 1] (SUT/count-matches "B" "B")))
+  (is (= ['(true false) 1] (SUT/count-matches "A" "B"))))
+
+(def oh \0)
+(def one \1)
+
+(deftest make-binary-seq-test
+  (are [length digits result] (= result (SUT/make-binary-seq length digits))
+    6 5  '(\0 \0 \0 \1 \0 \1)
+    8 5 '(\0 \0 \0 \0 \0 \1 \0 \1)
+    2 1 '(\0 \1)
+    9 8 '(\0 \0 \0 \0 \0 \1 \0 \0 \0)
+    3 8 '(\0 \0 \0)
+    9 11 '(\0 \0 \0 \0 \0 \1 \0 \1 \1)
+    5 0 '(\0 \0 \0 \0 \0)))
+
+(deftest binary->letter-seq-test
+  (are [length digits result] (= result (SUT/binary->letter-seq length digits))
+    6 5 '(\A \A \A \B \A \B)
+    8 5 '(\A \A \A \A \A \B \A \B)
+    2 1 '(\A \B)
+    9 8 '(\A \A \A \A \A \B \A \A \A)
+    3 8 '(\A \A \A)
+    9 11 '(\A \A \A \A \A \B \A \B \B)
+    5 0 '(\A \A \A \A \A)))
